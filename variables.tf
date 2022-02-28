72e0a2e117bd2a94db1cf8d20d89eb3d1abb9c90
Abdulrahman Mohammed <abdmuhammad610@gmail.com>
2022-02-28 16:58:12 +0100

    Add Network variables and Templates

---
+++ variables.tf
@@ -0,0 +1,50 @@
+##################
+# VARIABLES
+#####################
+
+variable "region" {
+  type        = string
+  description = "AWS region"
+  default     = "us-east-1"
+}
+
+variable "consul_address" {
+  type        = string
+  description = "Address of Consul server"
+  default     = "127.0.0.1"
+}
+
+variable "consul_port" {
+  type        = number
+  description = "Port Consul server is listening on"
+  default     = 8500
+}
+
+variable "consul_datacenter" {
+  type        = string
+  description = "Name of Consul datacenter"
+  default     = "dc1"
+}
+
+# variable "subnet_count" {
+#   type        = number
+#   description = "Number of subnets to be created"
+#   default     = 2
+# }
+
+# variable "cidr_block" {
+#   type        = string
+#   description = "CIDR block size"
+#   default     = "10.0.0.0/16"
+# }
+
+# variable "private_subnets" {
+#   type        = list(any)
+#   description = "Private Subnets"
+# }
+
+# variable "public_subnets" {
+#   type        = list(any)
+#   description = "Public Subnets"
+# }
+
